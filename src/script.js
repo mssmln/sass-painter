813be64ef8ca57b6671308fdaa37e159c8c54919
Massimiliano <m.croci.ly@gmail.com>
2021-02-22 15:31:46 +0100

    init

---
+++ src/script.js
@@ -0,0 +1,3 @@
+// Layout di riferimento: https://www.painterartist.com/en/?link=wm
+// Non è importante fare tutto, bastano header, jumbotron e footer, anche senza dropdown.
+// Bonus: fare altre parti oltre header, jumbotron e footer.
